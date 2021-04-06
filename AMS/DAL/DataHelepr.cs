a57d364a48e62d4637d366de8a136c7808ee9199
phi.kieu <phi.kieu.insenco@gmail.com>
2021-04-06 17:31:57 +0700

    [TASK] Test mysql connection with project console

--- AMS/DAL/DataHelepr.cs
+++ AMS/DAL/DataHelepr.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL.Model;
 using DataConnector;
 
 namespace DAL
@@ -11,9 +12,28 @@ namespace DAL
     {
         public Database database;
 
-        public DataHelepr(Database database)
+        public DataHelepr(string connection, string providerName)
         {
-            this.database = database;
+            database = new Database(connection, providerName);
+        }
+
+        public void Run()
+        {
+            database.OpenSharedConnection();
+            var a = Sql.Builder.From("GlobalParameter");
+            try
+            {
+                var asd = database.Fetch<GlobalParameter>(a);
+                GlobalParameter b = new GlobalParameter();
+                b.ID = 2;
+                b.ApartmentID = "sda";
+                b.ParaName = "adsd";
+                database.Insert("GlobalParameter", "ID", b);
+            }
+            catch(Exception ex)
+            {
+                
+            }
         }
     }
 }
---
+++ AMS/TestConnection/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+namespace TestConnection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Server=localhost;Uid=root;Pwd=123456;Database=new_data";
+            DataHelepr dataHelepr = new DataHelepr(connectionString,"Mysql");
+            dataHelepr.Run();
+        }
+    }
+}
